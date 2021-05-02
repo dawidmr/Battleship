8a4c94a114a948b1cecbdf513f93741917947c7c
Dawid Mroczek <dawid.mroczek@altium.com>
2021-05-02 12:47:35 +0200

    Grid tests

---
+++ Battleship.Tests/GridTests.cs
@@ -0,0 +1,37 @@
+﻿using Battleship.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Battleship.Tests
+{
+    [TestClass]
+    public class GridTests
+    {
+        private SquareStates[,] GetSquares(int size) => new SquareStates[size, size];
+
+        [TestMethod]
+        public void IsAnyVirginSquareTest_Negative()
+        {
+            var grid = PrepareTestGridWithShip(1);
+            var result = grid.IsAnyVirginSquare();
+
+            Assert.IsFalse(result, $"There shouldn't be any square with state {SquareStates.Virgin}.");
+        }
+
+        [TestMethod]
+        public void IsAnyVirginSquareTest_Positive()
+        {
+            var grid = PrepareTestGridWithShip(2);
+            var result = grid.IsAnyVirginSquare();
+
+            Assert.IsTrue(result, $"There should be at least one square with state {SquareStates.Virgin}.");
+        }
+
+        private TestGrid PrepareTestGridWithShip(int size)
+        {
+            var grid = new TestGrid(size, null, null);
+            grid.SetSquareState(0, 0, SquareStates.Ship);
+
+            return grid;
+        }
+    }
+}
---
+++ Battleship.Tests/TestGrid.cs
@@ -0,0 +1,18 @@
+﻿using Battleship.Game.Grids;
+using Battleship.Game.Interfaces;
+using Battleship.Models;
+
+namespace Battleship.Tests
+{
+    public class TestGrid : Grid
+    {
+        public TestGrid(int size, IFillStrategy fillStrategy, ISquareStateTransition squareStateTransitions) : base(size, fillStrategy, squareStateTransitions)
+        {
+        }
+
+        public void SetSquareState(int x, int y, SquareStates newState)
+        {
+            Squares[x, y] = newState;
+        }
+    }
+}
